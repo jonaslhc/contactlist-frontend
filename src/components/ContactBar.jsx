6d48aba9a81424ba61865dbae5f61c9bcebea0f3
Jonas Liao <jonas.liao@mail.utoronto.ca>
2018-03-15 21:47:00 -0700

    Refactored graphql fetching & added styling to contact card

--- src/components/ContactBar.jsx
+++ src/components/ContactBar.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { Container, InputGroup, Input } from 'reactstrap';
 import ContactCard from '../components/ContactCard';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import '../styles/contactBars.css';
+
+export const contactListQuery = gql`
+  query contactListQuery {
+    Contact {
+      contactId
+      firstname
+      lastname
+      address
+      phone
+      email
+    }
+  }
+`;
 
 class ContactBar extends Component {
   constructor(props) {
@@ -11,6 +26,32 @@ class ContactBar extends Component {
     this.state = {
 
     };
+    this.renderContacts = this.renderContacts.bind(this);
+  }
+
+  renderContacts(data) {
+    // return (
+    if (data.loading) {
+        return <p>Loading ...</p>;
+      }
+    if (data.error) {
+      return <p>{data.error.message}</p>;
+    }
+
+    return (
+      <div className="contacts-list">
+        {data.Contact.map(contact => (
+          <div key={contact.contactId} className="contact">
+            {contact.firstname}
+            {contact.lastname}
+            {contact.phone}
+            {contact.address}
+          </div>))
+        }
+      </div>
+    );
+    // };
+    // );
   }
 
   render() {
@@ -22,10 +63,11 @@ class ContactBar extends Component {
             <Input placeholder="Search" />
           </InputGroup>
         </div>
-        <ContactCard />
+        {/* <ContactCard /> */}
+        {this.renderContacts(this.props.data)}
       </Container>
     );
   }
 }
 
-export default ContactBar;
+export default graphql(contactListQuery)(ContactBar);
